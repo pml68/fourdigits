6feee5e02831a371d7f95c38ff06127e222ca0c3
pml68 <tutorialmester@gmail.com>
2024-02-14 09:59:20 +0100

    feat: add solution

---
+++ kimarado.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+fourDigitNumbers = [i for i in range(1111,10000)]
+
+noZeroFourDigitNumbers = [i for i in fourDigitNumbers if "0" not in str(i)]
+
+uniqueFourDigitNumbers = []
+
+for i in noZeroFourDigitNumbers:
+    amount = [0 for _ in range(9)]
+    for j in str(i):
+        amount[int(j)-1] += 1
+
+    works = True
+    for j in amount:
+        if j > 1:
+            works = False
+
+    if works:
+        uniqueFourDigitNumbers.append(i)
+
+pairs = []
+
+for i in uniqueFourDigitNumbers:
+    for j in uniqueFourDigitNumbers:
+        works = True
+        for k in range(len(str(i))):
+            if str(i)[k] in str(j):
+                works = False
+                break
+            else:
+                continue
+
+        if works:
+            pairs.append([i, j])
+
+digits = "123456789"
+
+for pair in pairs:
+    shoudEqual = 0
+    for i in digits:
+        if i not in str(pair[0])+str(pair[1]):
+            shoudEqual = int(i)
+            break
+
+    if pair[0] / pair[1] == shoudEqual:
+        print(f'Found it! It\'s {pair[0]}/{pair[1]}, which is equal to {shoudEqual}')
