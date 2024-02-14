526c46c2d81abdc650f8ea5135884ea049f2a88a
pml68 <tutorialmester@gmail.com>
2024-02-14 11:25:27 +0100

    bugfix: optimize unoptimized code

--- kimarado.py
+++ kimarado.py
@@ -1,47 +1,47 @@
 #!/bin/python3
 
-fourDigitNumbers = [i for i in range(1111,10000)]
+a = [i for i in range(1111,10000)]
 
-noZeroFourDigitNumbers = [i for i in fourDigitNumbers if "0" not in str(i)]
+b = [i for i in a if "0" not in str(i)]
 
-uniqueFourDigitNumbers = []
+c = []
 
-for i in noZeroFourDigitNumbers:
-    amount = [0 for _ in range(9)]
+for i in b:
+    d = [0 for _ in range(9)]
     for j in str(i):
-        amount[int(j)-1] += 1
+        d[int(j)-1] += 1
 
-    works = True
-    for j in amount:
+    w = True
+    for j in d:
         if j > 1:
-            works = False
+            w = False
 
-    if works:
-        uniqueFourDigitNumbers.append(i)
+    if w:
+        c.append(i)
 
-pairs = []
+ps = []
 
-for i in uniqueFourDigitNumbers:
-    for j in uniqueFourDigitNumbers:
-        works = True
+for i in c:
+    for j in c:
+        w = True
         for k in range(len(str(i))):
             if str(i)[k] in str(j):
-                works = False
+                w = False
                 break
             else:
                 continue
 
-        if works:
-            pairs.append([i, j])
+        if w:
+            ps.append([i, j])
 
-digits = "123456789"
+e = "123456789"
 
-for pair in pairs:
-    shoudEqual = 0
-    for i in digits:
-        if i not in str(pair[0])+str(pair[1]):
-            shoudEqual = int(i)
+for p in ps:
+    f = 0
+    for i in e:
+        if i not in str(p[0])+str(p[1]):
+            f = int(i)
             break
 
-    if pair[0] / pair[1] == shoudEqual:
-        print(f'Found one! It\'s {pair[0]}/{pair[1]}, which is equal to {shoudEqual}')
+    if p[0] / p[1] == f:
+        print(f'Found one! It\'s {p[0]}/{p[1]}, which is equal to {f}')
