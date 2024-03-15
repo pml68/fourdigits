7f51618cfb4c3378761f262ca51141823a6aba7f
pml68 <tutorialmester@gmail.com>
2024-03-15 22:59:04 +0100

    style: add comments

--- kimarado.py
+++ kimarado.py
@@ -1,29 +1,37 @@
 #!/bin/python3
 
+# Az összes négyjegyű szám, kivéve amiben tutira van 0
 a = [i for i in range(1111,10000)]
 
+# Az összes nullát tartalmazó szám eldobása
 b = [i for i in a if "0" not in str(i)]
 
+# Az összes egyedi szám
 c = []
 
 for i in b:
+    # Megszámoljuk, hányszor fordul elő egy-egy számjegy az adott számban
     d = [0 for _ in range(9)]
     for j in str(i):
         d[int(j)-1] += 1
 
     w = True
+    # Ha egynél többször, a számot nem rakjuk be az egyedi számok listájába
     for j in d:
         if j > 1:
             w = False
 
+    # Ha nem, belerakjuk
     if w:
         c.append(i)
 
+# Egyedi számpárok listája
 ps = []
 
 for i in c:
     for j in c:
         w = True
+        # Ha a második szám tartalmazza az első szám bármelyik számjegyét, nem helyezzük az egyedi párok listájába
         for k in range(len(str(i))):
             if str(i)[k] in str(j):
                 w = False
@@ -31,17 +39,21 @@ for i in c:
             else:
                 continue
 
+        # Ha nem, belehelyezzük
         if w:
             ps.append([i, j])
 
+# Az összes számjegy egy string-ben lista helyett
 e = "123456789"
 
 for p in ps:
     f = 0
     for i in e:
+        # Eredményként vesszük azt a számjegyet, amely nem szerepel a pár egyik számában sem
         if i not in str(p[0])+str(p[1]):
             f = int(i)
             break
 
+    # Ha tényleg ez az osztás eredménye, megtaláltunk egy megfelelő párt
     if p[0] / p[1] == f:
         print(f'Found one! It\'s {p[0]}/{p[1]}, which is equal to {f}')
